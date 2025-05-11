2fd9f3a0af18be6e93aac2388006bcaed002bc02
Medina Akhundova <medina.mstudio@gmail.com>
2025-05-11 16:09:41 +0400

    Добавлены новые тесты для zip

---
+++ src/test/java/guru/qa/FilesParsingTest.java
@@ -0,0 +1,138 @@
+package guru.qa;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.opencsv.CSVReader;
+import guru.qa.model.Glossary;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
+import java.util.Objects;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class FilesParsingTest {
+
+    private ClassLoader cl = FilesParsingTest.class.getClassLoader();
+    private static final Gson gson = new Gson();
+
+
+    @Test
+    void pdfFileParsingTest() throws Exception {
+        open("https://junit.org/junit5/docs/current/user-guide/");
+        File downloaded = $("[href*='junit-user-guide-5.12.2.pdf']").download();
+        PDF pdf = new PDF(downloaded);
+        //System.out.println();
+        Assertions.assertEquals("Stefan Bechtold, Sam Brannen, Johannes Link, Matthias Merdes, Marc Philipp, Juliette de Rancourt, Christian Stein", pdf.author);
+
+    }
+
+    @Test
+    void xlsFileParsingTest() throws Exception {
+        open("https://excelvba.ru/programmes/Teachers");
+        File downloaded = $("[href*='https://ExcelVBA.ru/sites/default/files/teachers.xls']").download();
+        XLS xls = new XLS(downloaded);
+        String actualValue = xls.excel.getSheetAt(0).getRow(3).getCell(2).getStringCellValue();
+        Assertions.assertTrue(actualValue.replaceAll("\\s+", " ")
+                .contains("Суммарное количество часов планируемое на штатную по всем разделам плана должно составлять примерно 1500 час в год"));
+
+    }
+
+    @Test
+    void csvFileParsingTest() throws Exception {
+        try (InputStream is = cl.getResourceAsStream("employees.csv");
+             CSVReader csvReader = new CSVReader(new InputStreamReader(is))) {
+            List<String[]> data = csvReader.readAll();
+            Assertions.assertEquals(2, data.size());
+            Assertions.assertArrayEquals(
+                    new String[]{"198", "Donald", "Connell", "DOCONNEL", "650", "21-JUN-07", "SH_CLERK"},
+                    data.get(0)
+            );
+            Assertions.assertArrayEquals(
+                    new String[]{"199", "Douglas", "Grant", "DGRANT", "650", "13-JAN-08", "SH_CLERK"},
+                    data.get(1)
+            );
+
+        }
+    }
+
+    @Test
+    void zipFileParsingTest() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("zipforTest.zip")
+        )) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println(entry.getName());
+            }
+
+
+        }
+
+
+    }
+
+    // dlya json
+    @Test
+    void jsonFileParsingTest() throws Exception {
+        try (Reader reader = new InputStreamReader(
+                cl.getResourceAsStream("glossary.json")
+        )) {
+            JsonObject actual = gson.fromJson(reader, JsonObject.class);
+            Assertions.assertEquals("example glossary", actual.get("title").getAsString());
+            Assertions.assertEquals(4654654, actual.get("ID").getAsInt());
+
+            JsonObject inner = actual.get("glossary").getAsJsonObject();//vlojenniy obyeckt
+            Assertions.assertEquals("SGML", inner.get("SortAs").getAsString());
+            Assertions.assertEquals("Standard Generalized Markup Language", inner.get("GlossTerm").getAsString());
+
+        }
+
+    }
+
+    @Test
+    void jsonFileParsingImprovedTest() throws Exception {
+        try (Reader reader = new InputStreamReader(
+                cl.getResourceAsStream("glossary.json")
+        )) {
+            Glossary actual = gson.fromJson(reader, Glossary.class);
+            Assertions.assertEquals("example glossary", actual.getTitle());
+            Assertions.assertEquals(4654654, actual.getID());
+            Assertions.assertEquals("SGML", actual.getGlossary().getSortAs());
+            Assertions.assertEquals("Standard Generalized Markup Language", actual.getGlossary().getGlossTerm());
+
+        }
+
+    }
+
+
+    @Test
+    void zipFileParsingTest3() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("zipTestHW9.zip")
+        )) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                System.out.println(entry.getName());
+            }
+
+
+        }
+
+
+    }
+
+}
+
